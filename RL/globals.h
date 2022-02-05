ca32327cb7e7032526551f2067bae5f90ef5f672
jacksonavery <58484091+jacksonavery@users.noreply.github.com>
2022-02-04 21:34:56 -0500

    adjust def font

--- RL/globals.h
+++ RL/globals.h
@@ -3,5 +3,5 @@
 namespace globals {
 	const int width = 1280, height = 720, tileSize = 40;
 	const int tWidth = width / tileSize, tHeight = height / tileSize;
-	const std::string font = "fonts/SourceHanSerif-VF.ttf";
+	const std::string font = "fonts/PixelMplus10-Regular.ttf";
 }
