16c10cce642b490533a05e895db0433b48b9cadf
jacksonavery <58484091+jacksonavery@users.noreply.github.com>
2022-02-08 23:55:52 -0500

    adjust default values

--- RL/globals.h
+++ RL/globals.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <string>
 namespace globals {
-	const int width = 1280, height = 720, tileSize = 40;
+	const int width = 1920, height = 1080, tileSize = 24;
 	const int tWidth = width / tileSize, tHeight = height / tileSize;
-	const std::string font = "fonts/PixelMplus10-Regular.ttf";
+	const std::string font = "fonts/PixelMplus12-Regular.ttf";
+	//const std::string font = "fonts/SourceHanSerif-VF.ttf";
 }
--- RL/tile.cpp
+++ RL/tile.cpp
@@ -3,7 +3,7 @@
 Tile::Tile() {
 	fgcolor = SDL_Color{ 255, 255, 255, 255 };
 	bgcolor = SDL_Color{ 0, 0, 0, 255 };
-	character = (Uint16) u'.';
+	character = (Uint16) u'ÅE';
 }
 Tile::Tile(Uint16 character, SDL_Color fgcolor, SDL_Color bgcolor) :
 	fgcolor(fgcolor), bgcolor(bgcolor), character(character) {}
