3ceab4c59edd08e4af37bcaf67d30225fc02edd8
jacksonavery <58484091+jacksonavery@users.noreply.github.com>
2022-02-11 01:23:19 -0500

    solidify target resolution

--- RL/globals.h
+++ RL/globals.h
@@ -2,6 +2,7 @@
 #include <string>
 namespace globals {
 	const int width = 1920, height = 1080, tileSize = 24;
+	//80*45 atm
 	const int tWidth = width / tileSize, tHeight = height / tileSize;
 	const std::string font = "fonts/PixelMplus12-Regular.ttf";
 	//const std::string font = "fonts/SourceHanSerif-VF.ttf";
