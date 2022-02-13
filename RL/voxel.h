4d5833c5285dfd2dfd08e32df3ada943c250e0d8
jacksonavery <58484091+jacksonavery@users.noreply.github.com>
2022-02-12 19:18:27 -0500

    another color update

--- RL/voxel.h
+++ RL/voxel.h
@@ -2,7 +2,7 @@
 #include "tile.h"
 
 struct Voxel {
-	Voxel(Tile* top = &Tile(0x25A1), Tile* side = &Tile(0x5182));
+	Voxel(Tile* top = &Tile(0x71d5), Tile* side = &Tile(0x5182));
 
 	Tile topTile, sideTile;
 };
