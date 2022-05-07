bad0a9e7f9334356a09264df51de5de2eb2fa795
jacksonavery <58484091+jacksonavery@users.noreply.github.com>
2022-05-07 12:14:02 -0400

    Update coordHelper.cpp

--- RL/coordHelper.cpp
+++ RL/coordHelper.cpp
@@ -7,17 +7,17 @@ bool coordHelper::areValidCoords(int x, int y, int w, int h) {
 	return true;
 }
 
-std::vector<Tile*> coordHelper::getSubset(int x, int y, int w, int h, std::vector<Tile>* tiles, int tw, int th) {
-	if (!areValidCoords(x, y, tw, th))
-		return std::vector<Tile*>();
-	if (!areValidCoords(x+w, y+h, tw, th))
-		return std::vector<Tile*>();
-	
-	std::vector<Tile*> ret;
-	for (int j = 0; j < h; j++) {
-		for (int i = 0; i < w; i++) {
-			ret.push_back(&tiles->at(x + i + (y + j) * w));
-		}
-	}
-	return ret;
-}
+//std::vector<Tile*> coordHelper::getSubset(int x, int y, int w, int h, std::vector<Tile>* tiles, int tw, int th) {
+//	if (!areValidCoords(x, y, tw, th))
+//		return std::vector<Tile*>();
+//	if (!areValidCoords(x+w, y+h, tw, th))
+//		return std::vector<Tile*>();
+//	
+//	std::vector<Tile*> ret;
+//	for (int j = 0; j < h; j++) {
+//		for (int i = 0; i < w; i++) {
+//			ret.push_back(&tiles->at(x + i + (y + j) * w));
+//		}
+//	}
+//	return ret;
+//}
