cc15d038de300430f309efc66b5e4ff198c5d259
jacksonavery <58484091+jacksonavery@users.noreply.github.com>
2022-05-07 12:14:16 -0400

    Update coordHelper.h

--- RL/coordHelper.h
+++ RL/coordHelper.h
@@ -7,5 +7,5 @@ struct coordHelper {
 	static bool areValidCoords(int x, int y, int w, int h);
 	// return a square subset of a set of tiles. error checks. tw and th are of the initial tileset.
 	// returns an empty vector on failure.
-	static std::vector<Tile*> getSubset(int x, int y, int w, int h, std::vector<Tile>* tiles, int tw, int th);
+	//static std::vector<Tile*> getSubset(int x, int y, int w, int h, std::vector<Tile>* tiles, int tw, int th);
 };
